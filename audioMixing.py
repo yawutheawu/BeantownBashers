6b72259cde1a73eaf62e8a89d6fd54e598136bc4
Derek Johnson <92835682+djkarate1@users.noreply.github.com>
2023-04-01 13:50:44 -0400

    Add files via upload

---
+++ audioMixing.py
@@ -0,0 +1,33 @@
+import pygame
+import time
+import beatTimer
+import beatMaps
+
+
+def playFile(song):
+    pygame.mixer.init()
+    map = beatMaps.getSong(song)
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play()
+    begin = int(time.time() * 1000)
+    length = map[0][-1]
+    index = 0
+    for i in range(0, length):
+        print(i)
+        if i in map[0]:
+            hit = beatTimer.inputBeat(map[1][0], (0.353*9))
+            if(hit):
+                time.sleep(3)
+                pass;
+            else:
+                return False
+            index += 1
+    return True
+
+
+if playFile("reg_enemy.mp3"):
+    print("Good Job")
+else:
+    print("Failed")
+
---
+++ beatMaps.py
@@ -0,0 +1,6 @@
+song1 = [[3, 6, 9, 12, 15, 18, 21], ["w"]]
+def getSong(x):
+    if(x == "reg_enemy.mp3"):
+        return song1
+    else:
+        return "Not Found"
