31a7a4c45ad48d4dff9e08e8452ff46ad3b6f4ed
Maykl Yakubovsky <mayklprime@gmail.com>
2023-04-01 11:11:50 -0400

    Fixed main and added settings file

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import pygame as pg
 import sys
 from settings import *
 
+
 class Game:
-    def __init(selfself):
+    def __init__(self):
         pg.init()
-        self.screen = pg.display.set_mode(res)
+        self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
 
     def new_game(self):
         pass
+
+    def update(self):
+        pg.display.flip()
+        self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
         self.screen.fill('black')
