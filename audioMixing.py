97d930dcb7bd41ef338846e5b2494ecb3a93c8ab
Derek Johnson <92835682+djkarate1@users.noreply.github.com>
2023-04-01 16:06:16 -0400

    Add files via upload

--- audioMixing.py
+++ audioMixing.py
@@ -3,28 +3,36 @@ import time
 import beatTimer
 import beatMaps
 
-#song is "fileName.mp3"
+global_time = 0
 
+def getGlobalTime():
+    return global_time
+
+#song is "fileName.mp3"
 #when you want to play music in sync to shooting beans type in code when you want to do that action: audioMixing.playFile("name.mp3"))
 
 def playFile(song):
     pygame.mixer.init()
-    map = beatMaps.getSong(song)
-    pygame.mixer.music.load(song)
+    pygame.mixer.music.load(name)
     pygame.mixer.music.set_volume(0.5)
     pygame.mixer.music.play()
+    map = beatMaps.getSong(song)
     begin = int(time.time() * 1000)
     length = map[0][-1]
     final = begin + length
     index = 0
+    global_time = int(time.time() * 1000)
     for i in range(0, length):
+        global_time = int(time.time() * 1000)
         if(begin >= final):
             begin = int(time.time() * 1000)
             final = begin + length
             pygame.mixer.music.play()
+            global_time = int(time.time() * 1000)
         print(i)
         if i in map[0]:
             hit = beatTimer.inputBeat(map[1][0], (0.353*3))
+            global_time = int(time.time() * 1000)
             if(hit):
                 hit = False
                 time.sleep(3)
@@ -33,6 +41,7 @@ def playFile(song):
                 return False
             index += 1
     return True
+
 
 x = playFile("reg_enemy.mp3")
 
--- beatTimer.py
+++ beatTimer.py
@@ -1,12 +1,12 @@
 from time import time
 import keyboard
+import audioMixing
 
 def inputBeat(key, frame):
 	milli = frame * 1000
 	start = int(time() * 1000)
 	end = start + milli
 	#print("Press: \'" + key + "\'")
-	p = False
 	while(start <= end):
 		ans = keyboard.is_pressed(' ')
 		if(start >= end):
@@ -15,6 +15,13 @@ def inputBeat(key, frame):
 			ans = False
 			#print("Pressed")
 			return True
-
 		start = int(time() * 1000)
 	return False
+
+#Parameter:
+#audioMixing.getGlobalTime()
+def isOnTime(x):
+	if(x % 3 == 0):
+		return True
+	else:
+		return False
