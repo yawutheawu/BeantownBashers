1cc844b9af848356fcd2f332ac69a6184e31d4d9
Maykl Yakubovsky <mayklprime@gmail.com>
2023-04-01 13:31:16 -0400

    theft (literally (but we will change trust))

--- player.py
+++ player.py
@@ -49,13 +49,18 @@ class Player:
             self.y += dy
 
     def draw(self):
+        '''
         pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
                      (self.x*100 + WIDTH * math.cos(self.angle),
-                     self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+                     self.y * 100 + WIDTH * math.sin(self.angle)), 2)'''
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100),15)
 
     def update(self):
         self.movement()
+
+    @property
+    def pos(self):
+        return self.x, self.y
 
     @property
     def map_pos(self):
