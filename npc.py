13bbec9aa6dfdef6707397949ed98936ff04171c
Maykl Yakubovsky <mayklprime@gmail.com>
2023-04-01 16:56:34 -0400

    settings rebalance

--- npc.py
+++ npc.py
@@ -12,12 +12,12 @@ class NPC(AnimatedSprite):
         self.pain_images = self.get_images(self.path + '/pain')
         self.walk_images = self.get_images(self.path + '/walk')
 
-        self.attack_dist = randint(3, 6)
-        self.speed = 0.03
+        self.attack_dist = randint(1, 3)
+        self.speed = 0.01
         self.size = 20
-        self.health = 100
-        self.attack_damage = 10
-        self.accuracy = 0.15
+        self.health = 60
+        self.attack_damage = 5
+        self.accuracy = 0.05
         self.alive = True
         self.pain = False
         self.ray_cast_value = False
